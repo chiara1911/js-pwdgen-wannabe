f903f9af322e97cb063c312d4ad45c0e645f9d07
chiara1911 <chiaracesari89@gmail.com>
2023-10-06 16:55:43 +0200

    ticket

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+let divPassword = document.getElementById('newPassword');
+
+let nome= prompt('Qual è il tuo nome?' );
+let cognome = prompt('Qual è il tuo cognome?');
+let colore = prompt('Qual è il tuo colore preferito?');
+const numero = 21;
+
+
+
+
+
