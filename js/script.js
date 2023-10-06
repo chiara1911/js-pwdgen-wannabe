0faa42a61f6af85c4d3dc24c6e7e6e729f4c7cfb
chiara1911 <chiaracesari89@gmail.com>
2023-10-06 17:47:26 +0200

    add selected word

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 
 
-let divPassword = document.getElementById('newPassword');
+ let divPassword = document.getElementById('newPassword');
 
-let nome= prompt('Qual è il tuo nome?' );
+ let nome= prompt('Qual è il tuo nome?' );
 let cognome = prompt('Qual è il tuo cognome?');
 let colore = prompt('Qual è il tuo colore preferito?');
 const numero = 21;
 
 
-let newPassword = nome + cognome + colore + numero;
+ let newPassword = nome + cognome + colore + numero;
 
-divPassword.innerHTML = newPassword;
+ divPassword.innerHTML = newPassword;
 
+let divNome = document.getElementById('nome');
 
+divNome.innerHTML = nome;
+
+let divCognome = document.getElementById('cognome');
+ 
+divCognome.innerHTML = cognome;
+
+let divColore = document.getElementById('colore');
+
+divColore.innerHTML = colore;
